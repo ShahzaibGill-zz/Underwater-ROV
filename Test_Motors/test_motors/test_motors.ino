e0954c0a5cbe8668055946ee305e4b9d6c04eace
Shahzaib Gill <shahzaibgill@gmail.com>
2017-11-11 21:20:50 -0500

    Adding code for 10DOF IMU

--- Test_Motors/test_motors/test_motors.ino
+++ Test_Motors/test_motors/test_motors.ino
@@ -7,26 +7,28 @@ Servo MOTOR2;
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  MOTOR1.attach(12);
-  MOTOR2.attach(11);
+  MOTOR1.attach(9);
+//  MOTOR2.attach(11);
+  delay(4000);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  run_motors(150,150);
+  run_motors(1300,1300);
   delay(4000);
-  run_motors(90,90);
+  run_motors(1500,1500);
   delay(4000);
-  run_motors(30,30);
+  run_motors(1600,1600);
   delay(4000);
-  run_motors(90,90);
+  run_motors(1500,1500);
+  delay(10000);
 }
 
 
 void run_motors(int m1val, int m2val)
 {
   MOTOR1.write(m1val);
-  MOTOR2.write(m2val);
+//  MOTOR2.writeMicroseconds(m2val);
 
 }
