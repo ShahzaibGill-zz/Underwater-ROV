b0f506697406a3fa281803d715549f27acb3acd1
codyreading <codyreading@gmail.com>
2017-11-17 00:07:27 -0500

    Added test code for RS485 communication

---
+++ test_files/test_rs485/test_RS485_receive/test_RS485_receive.ino
@@ -0,0 +1,42 @@
+/*
+   - Pin 0 -> RO
+   - 5V, GND -> VCC, GND
+   - Open Serial Monitor, type in top window.
+   - Other Serial Monitor will display value sent
+*/
+
+/*-----( Import needed libraries )-----*/
+#include <SoftwareSerial.h>
+/*-----( Declare Constants and Pin Numbers )-----*/
+#define SSerialRX        10  //Serial Receive pin
+#define SSerialTX        11  //Serial Transmit pin
+
+/*-----( Declare objects )-----*/
+SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+
+/*-----( Declare Variables )-----*/
+int byteReceived;
+int byteSend;
+
+void setup()   /****** SETUP: RUNS ONCE ******/
+{
+  // Start the built-in serial port, probably to Serial Monitor
+  Serial.begin(9600);
+  Serial.println("SerialRemote");  // Can be ignored
+
+  // Start the software serial port, to another device
+  RS485Serial.begin(4800);   // set the data rate
+}//--(end setup )---
+
+
+void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+{
+  //Copy input data to output
+  if (RS485Serial.available())
+  {
+    byteReceived = RS485Serial.read();   // Read the byte
+    Serial.write(byteReceived);   // Show on Serial Monitor
+
+  }// End If RS485SerialAvailable
+
+}//--(end main loop )---
---
+++ test_files/test_rs485/test_RS485_transmit/test_RS485_transmit.ino
@@ -0,0 +1,45 @@
+/*
+   - Pin 1 -> DI
+   - 5V, GND -> VCC, GND
+   - Open Serial Monitor, type in top window.
+   - Other Serial Monitor will display value sent
+*/
+
+/*-----( Import needed libraries )-----*/
+#include <SoftwareSerial.h>
+/*-----( Declare Constants and Pin Numbers )-----*/
+#define SSerialRX        10  //Serial Receive pin
+#define SSerialTX        11  //Serial Transmit pin
+
+/*-----( Declare objects )-----*/
+SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
+
+/*-----( Declare Variables )-----*/
+int byteReceived;
+int byteSend;
+
+void setup()   /****** SETUP: RUNS ONCE ******/
+{
+  // Start the built-in serial port, probably to Serial Monitor
+  Serial.begin(9600);
+  Serial.println("YourDuino.com SoftwareSerial remote loop example");
+  Serial.println("Use Serial Monitor, type in upper window, ENTER");
+
+  // Start the software serial port, to another device
+  RS485Serial.begin(4800);   // set the data rate
+
+}//--(end setup )---
+
+
+void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+{
+  if (Serial.available())
+  {
+    byteReceived = Serial.read();
+
+    RS485Serial.write(byteReceived);          // Send byte to Remote Arduino
+  }
+
+
+}//--(end main loop )---
+
